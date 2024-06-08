0c625b1a740cc06907a20fa0a2951b5b91e0c5fe
Marius Alexandersen <marius_kule@hotmail.com>
2024-06-09 01:22:47 +0200

    build script fixes

--- build.bat
+++ build.bat
@@ -4,7 +4,8 @@ set CFLAGS=-Wextra -Wall -std=c11 -pedantic -O2 -mwindows
 set SRC=%~dp0src\
 set OUT=%~dp0out\
 
-del /Q out
 mkdir out 2> NUL
+del /Q out
+
 windres %SRC%icon.rc -o %OUT%icon.o
 gcc -o %OUT%tlf.exe %SRC%main.c %OUT%icon.o %CFLAGS% 
