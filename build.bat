a96abced5642ac87c3775e9fe4461a19baf564d2
Marius Alexandersen <marius_kule@hotmail.com>
2024-06-09 00:15:06 +0200

    build

--- build.bat
+++ build.bat
@@ -1,7 +1,10 @@
+@echo off
+
 set CFLAGS=-Wextra -Wall -std=c11 -pedantic -O2 -mwindows
 set SRC=%~dp0src\
 set OUT=%~dp0out\
 
 del /Q out
+mkdir out 2> NUL
 windres %SRC%icon.rc -o %OUT%icon.o
 gcc -o %OUT%tlf.exe %SRC%main.c %OUT%icon.o %CFLAGS% 
