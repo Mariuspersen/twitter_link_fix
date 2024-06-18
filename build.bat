371c356dfe7262698cf04d8c145eb4cdac3e9ceb
Mariuspersen <59454368+Mariuspersen@users.noreply.github.com>
2024-06-18 09:36:50 +0200

    Update build.bat

--- build.bat
+++ build.bat
@@ -4,8 +4,8 @@ set CFLAGS=-Wextra -Wall -std=c11 -pedantic -O2 -mwindows
 set SRC=%~dp0src\
 set OUT=%~dp0out\
 
-mkdir out 2> NUL
-del /Q out
+mkdir %OUT% 2> NUL
+del /Q %OUT%
 
 windres %SRC%icon.rc -o %OUT%icon.o
 gcc -o %OUT%tlf.exe %SRC%main.c %OUT%icon.o %CFLAGS% 
